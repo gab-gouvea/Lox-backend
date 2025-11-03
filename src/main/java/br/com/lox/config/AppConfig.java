9582f394c2f74a5e99487acc814b9ad6a3b10e52
gabriel <oliveirag396@gmail.com>
2025-11-03 13:04:00 -0300

    add: cors for frontend

---
+++ src/main/java/br/com/lox/config/AppConfig.java
@@ -0,0 +1,19 @@
+package br.com.lox.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
